6deb9fddfa6b8299d19b0d049404220140f157f0
Nestor Custodio <sakimorix@gmail.com>
2019-10-22 00:30:22 -0500

    add: initial "autoregister" support

---
+++ lib/env_parser/autoregister.rb
@@ -0,0 +1,21 @@
+require 'env_parser'
+require 'psych'
+
+begin
+  auto_register_file = '.env_parser.yml'
+  auto_register_spec = Psych.load_file(auto_register_file)
+
+  auto_register_spec.deep_symbolize_keys!
+  auto_register_spec.transform_values! { |spec| spec.merge as: spec[:as]&.to_sym }
+  EnvParser.register auto_register_spec
+
+## Psych raises an Errno::ENOENT on file-not-found.
+##
+rescue Errno::ENOENT
+  raise EnvParser::AutoRegisterFileNotFound, %(file not found: "#{auto_register_file}")
+
+## Psych raises a Psych::SyntaxError on unparseable YAML.
+##
+rescue Psych::SyntaxError => e
+  raise EnvParser::UnparseableAutoRegisterSpec, "malformed YAML in spec file: #{e.message}"
+end
