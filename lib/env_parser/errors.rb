b5743ad53a30593cc088b20c5044c3cadda99e82
Nestor Custodio <sakimorix@gmail.com>
2022-12-18 20:25:25 -0600

    rollup: switch to single-'#' comments

--- lib/env_parser/errors.rb
+++ lib/env_parser/errors.rb
@@ -1,40 +1,40 @@
 class EnvParser
-  ## Base error class for EnvParser.
-  ##
+  # Base error class for EnvParser.
+  #
   class Error < ::StandardError
   end
 
-  ## Error class used to indicate a type has already been defined.
-  ##
+  # Error class used to indicate a type has already been defined.
+  #
   class TypeAlreadyDefinedError < Error
   end
 
-  ## Error class used to indicate the requested "as" type has not been defined.
-  ##
+  # Error class used to indicate the requested "as" type has not been defined.
+  #
   class UnknownTypeError < Error
   end
 
-  ## Error class used to indicate value given is not convertible to the requested type.
-  ##
+  # Error class used to indicate value given is not convertible to the requested type.
+  #
   class ValueNotConvertibleError < Error
   end
 
-  ## Error class used to indicate parsed values that do not pass user-validation, either by not
-  ## being part of the given "from_set" list, or by failing the "validated_by" Proc or yield-block
-  ## check.
-  ##
+  # Error class used to indicate parsed values that do not pass user-validation, either by not
+  # being part of the given "from_set" list, or by failing the "validated_by" Proc or yield-block
+  # check.
+  #
   class ValueNotAllowedError < Error
   end
 
-  ## Error class used to indicate a missing auto-registration spec file (used by the "autoregister"
-  ## feature).
-  ##
+  # Error class used to indicate a missing auto-registration spec file (used by the "autoregister"
+  # feature).
+  #
   class AutoregisterFileNotFound < Error
   end
 
-  ## Error class used to indicate an unparseable auto-registration spec (used by the "autoregister"
-  ## feature).
-  ##
+  # Error class used to indicate an unparseable auto-registration spec (used by the "autoregister"
+  # feature).
+  #
   class UnparseableAutoregisterSpec < Error
   end
 end
--- lib/env_parser/types/base_types.rb
+++ lib/env_parser/types/base_types.rb
@@ -1,75 +1,75 @@
 require 'env_parser'
 
-## The parent module for all EnvParser type definition modules.
-## Exists only for documentation's sake.
-##
+# The parent module for all EnvParser type definition modules.
+# Exists only for documentation's sake.
+#
 module EnvParser::Types
-  ## Defines types for primitive classes, adding the following:
-  ##
-  ## <table>
-  ##   <tbody>
-  ##     <tr>
-  ##       <th><code>:as</code> value</th>
-  ##       <th>type returned</th>
-  ##       <th>default</th>
-  ##       <th>notes</th>
-  ##     </tr>
-  ##   </tbody>
-  ##   <tbody>
-  ##     <tr>
-  ##       <td>:string</td>
-  ##       <td>String</td>
-  ##       <td><code>''</code></td>
-  ##       <td></td>
-  ##     </tr>
-  ##     <tr>
-  ##       <td>:symbol</td>
-  ##       <td>Symbol</td>
-  ##       <td><code>:''</code></td>
-  ##       <td></td>
-  ##     </tr>
-  ##     <tr>
-  ##       <td>:boolean</td>
-  ##       <td>TrueValue / FalseValue</td>
-  ##       <td><code>false</code></td>
-  ##       <td></td>
-  ##     </tr>
-  ##     <tr>
-  ##       <td>:int / :integer</td>
-  ##       <td>Integer</td>
-  ##       <td><code>0</code></td>
-  ##       <td></td>
-  ##     </tr>
-  ##     <tr>
-  ##       <td>:float / :decimal / :number</td>
-  ##       <td>Float</td>
-  ##       <td><code>0.0</code></td>
-  ##       <td></td>
-  ##     </tr>
-  ##     <tr>
-  ##       <td>:json</td>
-  ##       <td>&lt; depends on JSON given &gt;</td>
-  ##       <td><code>nil</code></td>
-  ##       <td></td>
-  ##     </tr>
-  ##     <tr>
-  ##       <td>:array</td>
-  ##       <td>Array</td>
-  ##       <td><code>[]</code></td>
-  ##       <td></td>
-  ##     </tr>
-  ##     <tr>
-  ##       <td>:hash</td>
-  ##       <td>Hash</td>
-  ##       <td><code>{}</code></td>
-  ##       <td></td>
-  ##     </tr>
-  ##   </tbody>
-  ## </table>
-  ##
-  ## Note JSON is parsed using *quirks-mode* (meaning 'true', '25', and 'null' are all considered
-  ## valid, parseable JSON).
-  ##
+  # Defines types for primitive classes, adding the following:
+  #
+  # <table>
+  #   <tbody>
+  #     <tr>
+  #       <th><code>:as</code> value</th>
+  #       <th>type returned</th>
+  #       <th>default</th>
+  #       <th>notes</th>
+  #     </tr>
+  #   </tbody>
+  #   <tbody>
+  #     <tr>
+  #       <td>:string</td>
+  #       <td>String</td>
+  #       <td><code>''</code></td>
+  #       <td></td>
+  #     </tr>
+  #     <tr>
+  #       <td>:symbol</td>
+  #       <td>Symbol</td>
+  #       <td><code>:''</code></td>
+  #       <td></td>
+  #     </tr>
+  #     <tr>
+  #       <td>:boolean</td>
+  #       <td>TrueValue / FalseValue</td>
+  #       <td><code>false</code></td>
+  #       <td></td>
+  #     </tr>
+  #     <tr>
+  #       <td>:int / :integer</td>
+  #       <td>Integer</td>
+  #       <td><code>0</code></td>
+  #       <td></td>
+  #     </tr>
+  #     <tr>
+  #       <td>:float / :decimal / :number</td>
+  #       <td>Float</td>
+  #       <td><code>0.0</code></td>
+  #       <td></td>
+  #     </tr>
+  #     <tr>
+  #       <td>:json</td>
+  #       <td>&lt; depends on JSON given &gt;</td>
+  #       <td><code>nil</code></td>
+  #       <td></td>
+  #     </tr>
+  #     <tr>
+  #       <td>:array</td>
+  #       <td>Array</td>
+  #       <td><code>[]</code></td>
+  #       <td></td>
+  #     </tr>
+  #     <tr>
+  #       <td>:hash</td>
+  #       <td>Hash</td>
+  #       <td><code>{}</code></td>
+  #       <td></td>
+  #     </tr>
+  #   </tbody>
+  # </table>
+  #
+  # Note JSON is parsed using *quirks-mode* (meaning 'true', '25', and 'null' are all considered
+  # valid, parseable JSON).
+  #
   module BaseTypes
     EnvParser.define_type(:string, if_unset: '') do |value|
       value
